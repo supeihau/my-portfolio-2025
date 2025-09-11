b2268d10973491a54ee875c82bb61f9a04dbc4d2
supeihau <selina240@gmail.com>
2025-09-11 23:00:27 +0800

    fix: 修改intro背景圖片

--- components/intro.vue
+++ components/intro.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-row class="px-lg-16 px-8 pb-16 bg-light-yellow">
-        <v-col cols="12" md="6" lg="6" class="d-flex align-center pa-0">
+    <v-row class="px-lg-16 px-8 custom-bg">
+        <v-col cols="12" md="6" lg="7" class="d-flex align-center pa-0">
             <div class="d-flex flex-column align-start justify-start">
                 <div class="text-brown"
                     :class="isDesktop ? 'text-h1-semi-bold': 'text-h2-semi-bold'">
@@ -11,14 +11,14 @@
                     應徵工作職稱
                 </div>
                 <div class="mt-5 text-brown"
-                    :class="isDesktop ? 'text-h4-regular': 'text-h6-regular'">
+                    :class="isDesktop ? 'text-h5-regular': 'text-h6-regular'">
                     我對設計一直抱有熱情，曾自學並產出一些平面設計作品；大學就讀資管系時，希望將設計興趣與專業能力結合，便開始進入UIUX介面設計的領域，並學習前端相關技術，讓設計稿能真正落地。一路以來，我不僅在意功是否能完成，更重視介面的設計感與使用者需求，同時注重程式碼的可讀性與擴充性。<br/>
                     目前我熟悉 Nuxt 3、React、Vue 與 Figma，並持續探索如何在需求、設計與程式之間找到平衡。未來，我希望透過更多實戰經驗，打造能說故事、解決問題、讓使用者喜愛的數位產品，同時也為團隊或品牌創造流量與實際成效。
                 </div>
             </div>
         </v-col>
-        <v-col cols="12" md="6" lg="6" class="d-flex align-center justify-end" :class="!isDesktop && 'mt-5'">
-            <img src="/assets/images/intro-pic.png" alt="intro-pic" style="width: 90%;"/>
+        <v-col cols="12" md="6" lg="5" class="d-flex align-center justify-end" :class="!isDesktop && 'mt-5'">
+            <img src="/images/intro-pic.png" alt="intro-pic" style="width: 90%;"/>
         </v-col>
     </v-row>
 </template>
@@ -30,4 +30,12 @@ const { isDesktop } = useInnerWidth();
 </script>
 
 <style lang="scss" scoped>
+.custom-bg {
+    background-image: url('/images/intro-bg.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    padding-top: 100px;
+    padding-bottom: 100px;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,7 @@
   <v-app>
     <NavBar></NavBar>
     <v-main>
-      <v-container class="pa-0" fluid>
-        <div>
-          <v-row class="row d-flex flex-nowrap justify-space-between pa-0 bg-light-yellow" >
-            <img src="/assets/images/bg-left-top.png" alt="bg-left-top" class="w-100 w-md-50 h-auto intro-bg-left-top" />
-            <img src="/assets/images/bg-right-top.png" alt="bg-right-top" class="d-none d-md-flex intro-bg-right-top"/>
-          </v-row>
-          <div class="intro">
-            <intro></intro>
-          </div>
-          <v-row class="row d-flex flex-nowrap justify-space-between align-end pa-0 intro-bg-bottom">
-            <img src="/assets/images/bg-left-bottom.png" alt="bg-left-bottom" class="intro-bg-left-bottom"/>
-            <img src="/assets/images/bg-right-bottom.png" alt="bg-right-bottom" class="d-none d-md-flex intro-bg-right-bottom" />
-          </v-row>
-        </div>
-      </v-container>
+      <intro></intro>
     </v-main>
   </v-app>
 </template>
@@ -26,38 +12,4 @@ import NavBar from '@/components/navbar.vue';
 </script>
 
 <style scoped>
-.intro-bg-left-top {
-  top: -40px; 
-  position: relative;
-}
-
-.intro-bg-right-top {
-  width: auto;
-  height: 100px;
-  margin-right: 100px; 
-  margin-top: 50px;
-}
-
-.intro {
-  z-index: 1;
-}
-
-.intro-bg-bottom {
-  top: -350px; 
-  position: relative;
-}
-
-.intro-bg-left-bottom {
-  width: auto; 
-  height: 300px; 
-  margin-right: 100px; 
-  margin-bottom: 50px;
-}
-
-.intro-bg-right-bottom {
-  width: auto; 
-  height: 100px; 
-  margin-right: 100px; 
-  margin-bottom: 50px;
-}
 </style>
